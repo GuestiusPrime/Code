74c97b35985ef489df50cc53e4f65df61f3d37f5
Oliver <olimasad@gmail.com>
2025-07-29 17:19:48 -0400

    made michelle website

--- c#/helloworld/obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ c#/helloworld/obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c37540d1f6477dc39af0cae547e58fcecea8bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db191d9ba4289fd90cdd237f8b5ffcae1a85723")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
